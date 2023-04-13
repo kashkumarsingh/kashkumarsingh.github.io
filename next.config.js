8dad1fd700d3b4f44b8d2ae96b0d7cf1ad8b3d8b
Kashkumar Singh <kash.singh1989@gmail.com>
2023-04-13 20:55:32 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
-//const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
- // assetPrefix: isProd ? '/my-portfolio' : '',
- basePath: '/portfolio',
- assetPrefix: '/portfolio/',
- exportPathMap: function() {
-   return {
-     '/': { page: '/' },
-     //'/about': { page: '/about' },
-     // ...
-   };
- },
+  // assetPrefix: isProd ? '/my-portfolio' : '',
+  basePath: isProduction ? '/kashkumarsingh.github.io' : '',
+  assetPrefix: isProduction ? '/kashkumarsingh.github.io/' : '',
+  env: {
+    BASE_PATH: isProduction ? '/kashkumarsingh.github.io' : ''
+  },
+  trailingSlash: true,
+  // exportPathMap: function () {
+  //   return {
+  //     "/": { page: "/" },
+  //     //'/about': { page: '/about' },
+  //     // ...
+  //   };
+  // },
   reactStrictMode: true,
-  
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

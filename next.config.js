73927819ebb13e74e2f79f67e2532ca610c55cc6
Kashkumar Singh <kash.singh1989@gmail.com>
2023-04-13 18:22:02 +0100

    updated files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+//const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
+ // assetPrefix: isProd ? '/my-portfolio' : '',
+ basePath: '/portfolio',
+ assetPrefix: '/portfolio/',
+ exportPathMap: function() {
+   return {
+     '/': { page: '/' },
+     //'/about': { page: '/about' },
+     // ...
+   };
+ },
   reactStrictMode: true,
+  
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,17 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import Router, { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const basename = process.env.NODE_ENV === 'production' ? '/portfolio' : '';
   return (
-    <div className="page-wrapper">
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
+      <div className="page-wrapper">
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer year={new Date().getFullYear()} />
+      </div>
   )
 }
 
